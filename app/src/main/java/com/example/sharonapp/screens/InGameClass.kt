6bcff16a3c9182f7e401b4966bda5541c7b61e30
Gulgae <mars024024@gmail.com>
2024-12-02 16:55:18 +0900

    움직임 감지 threshold 15 -> 5

--- app/src/main/java/com/example/sharonapp/screens/InGameClass.kt
+++ app/src/main/java/com/example/sharonapp/screens/InGameClass.kt
@@ -101,7 +101,7 @@ class InGameClass {
             var xValue by remember { mutableFloatStateOf(0f) }
             var yValue by remember { mutableFloatStateOf(0f) }
             var zValue by remember { mutableFloatStateOf(0f) }
-            val threshold = 15f
+            val threshold = 5f
             
             val hasMotionDetected = nfcViewModel.hasMotionDetected.collectAsState()
             val isTagged = nfcViewModel.isTagged.collectAsState()
